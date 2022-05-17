32fff2df5841cb685e02fb43cbbbbcbee2cdf680
auhustsinovich <avgustinovich4@gmail.com>
2022-05-17 23:20:10 +0200

    chevrons settled up

--- ColorizedApp_SwiftUI/ContentView.swift
+++ ColorizedApp_SwiftUI/ContentView.swift
@@ -7,22 +7,25 @@
 
 import SwiftUI
 
+extension ContentView {
+    enum FieldType: Int {
+        case red, green, blue
+    }
+}
+
 struct ContentView: View {
     
     @State private var red = Double.random(in: 0...255)
     @State private var green = Double.random(in: 0...255)
     @State private var blue = Double.random(in: 0...255)
     
-    @FocusState private var isInputActive: Bool
+    @FocusState private var focusedField: FieldType?
     
     var body: some View {
         
         ZStack {
             Color(.init(red: 0.1, green: 0.4, blue: 0.2, alpha: 1))
                 .ignoresSafeArea()
-                .onTapGesture {
-                    isInputActive = false
-                }
             
             VStack(spacing: 20) {
                 
@@ -30,24 +33,27 @@ struct ContentView: View {
                 
                 VStack {
                     ColorSliderView(sliderValue: $red, color: .red)
+                        .focused($focusedField, equals: .red)
                     ColorSliderView(sliderValue: $green, color: .green)
+                        .focused($focusedField, equals: .green)
                     ColorSliderView(sliderValue: $blue, color: .blue)
+                        .focused($focusedField, equals: .blue)
                 }
                 .frame(height: 150)
-                .focused($isInputActive)
+            
                 .toolbar {
                     ToolbarItemGroup(placement: .keyboard) {
                         
                         Button(action: moveUp) {
                             Image(systemName: "chevron.up")
-                        }
+                        }.disabled(focusedField == .red ? true : false)
                         Button(action: moveDown) {
                             Image(systemName: "chevron.down")
-                        }
+                        }.disabled(focusedField == .blue ? true : false)
                         Spacer()
                         
                         Button("Done") {
-                            isInputActive = false
+                            focusedField = nil
                         }
                     }
                 }
@@ -58,11 +64,15 @@ struct ContentView: View {
     }
     
     private func moveUp() {
-        
+        focusedField = focusedField.map {
+            FieldType(rawValue: $0.rawValue - 1) ?? .red
+        }
     }
     
     private func moveDown() {
-        
+        focusedField = focusedField.map {
+            FieldType(rawValue: $0.rawValue + 1) ?? .blue
+        }
     }
 }
 
