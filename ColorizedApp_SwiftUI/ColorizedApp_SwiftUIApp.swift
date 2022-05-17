d606e23e15a145a13bdf84ddf87758df14485294
auhustsinovich <avgustinovich4@gmail.com>
2022-05-17 12:15:50 +0200

    Initial Commit

---
+++ ColorizedApp_SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp_SwiftUI
+//
+//  Created by Daniil Auhustsinovich on 17.05.22.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorizedApp_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorizedApp_SwiftUI
+//
+//  Created by Daniil Auhustsinovich on 17.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
